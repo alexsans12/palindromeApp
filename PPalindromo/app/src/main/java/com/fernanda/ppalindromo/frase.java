d8f4b65c2546af401fe4da45bbb279b626d20ea0
16220600 <83486041+16220600@users.noreply.github.com>
2021-05-03 21:39:20 -0600

    Add files via upload

---
+++ PPalindromo/app/src/main/java/com/fernanda/ppalindromo/frase.java
@@ -0,0 +1,14 @@
+package com.fernanda.ppalindromo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class frase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frase);
+    }
+}
---
+++ PPalindromo/app/src/main/java/com/fernanda/ppalindromo/respuesta1.java
@@ -0,0 +1,14 @@
+package com.fernanda.ppalindromo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class respuesta1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_respuesta1);
+    }
+}
