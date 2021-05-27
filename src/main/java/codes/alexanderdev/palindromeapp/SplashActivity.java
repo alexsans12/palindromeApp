d8bd820c29ed65e9a55ebdb26a3caffc3790af0d
Eddy Alexander <56743574+alexsans12@users.noreply.github.com>
2021-05-26 23:55:34 -0600

    Nuevos cambios

---
+++ src/main/java/codes/alexanderdev/palindromeapp/SplashActivity.java
@@ -0,0 +1,30 @@
+package codes.alexanderdev.palindromeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    static final long SPLASH_SCREEN_DELAY = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(timerTask, SPLASH_SCREEN_DELAY);
+    }
+}
---
+++ src/main/java/codes/alexanderdev/palindromeapp/frase.java
@@ -0,0 +1,14 @@
+package codes.alexanderdev.palindromeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class frase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frase);
+    }
+}
